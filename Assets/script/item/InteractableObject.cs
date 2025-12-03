b118347046e304d27fdbe733b34ddb48bcbe43ef
HaFo64 <trihao331166@gmail.com>
2025-12-04 03:52:29 +0700

    ccUnity
    :))))

--- Assets/script/item/InteractableObject.cs
+++ Assets/script/item/InteractableObject.cs
@@ -8,96 +8,10 @@ public class InteractableObject : MonoBehaviour
 
     [Header("Cài đặt")]
     public float holdTime = 2f;
-    //private float currentHoldTime = 0f;
-    //private bool isPlayerNearby = false;
-    //private bool isCollected = false;
-
-    //private bool isHintShowing = false;
-
-    //void Update()
-    //{
-    //    if (isPlayerNearby && !isCollected)
-    //    {
-    //        if (!GameManager.instance.isAiming)
-    //        {
-    //            // Nếu đang loading dở thì hủy
-    //            if (currentHoldTime > 0)
-    //            {
-    //                currentHoldTime = 0f;
-    //                GameManager.instance.StopLoading();
-    //            }
-
-    //            // Ẩn gợi ý nếu đang hiện
-    //            if (isHintShowing)
-    //            {
-    //                GameManager.instance.HideHint();
-    //                isHintShowing = false;
-    //            }
-
-    //            return; // Dừng code tại đây, không cho làm gì thêm
-    //        }
-
-    //        if (!isHintShowing)
-    //        {
-    //            ShowInteractionHint();
-    //            isHintShowing = true;
-    //        }
-
-    //        // Logic cửa: Nếu là cửa và chưa đủ đồ thì không cho giữ E
-    //        if (type == ObjectType.Door && GameManager.instance.currentItems < 3) return;
-
-    //        // Logic giữ E (như cũ)
-    //        if (Input.GetKey(KeyCode.E))
-    //        {
-    //            currentHoldTime += Time.deltaTime;
-    //            GameManager.instance.UpdateLoading(currentHoldTime, holdTime);
-
-    //            if (currentHoldTime >= holdTime)
-    //            {
-    //                PerformAction();
-    //            }
-    //        }
-    //        else
-    //        {
-    //            if (currentHoldTime > 0)
-    //            {
-    //                currentHoldTime = 0f;
-    //                GameManager.instance.StopLoading();
-    //            }
-    //        }
-
-    //        if (type == ObjectType.Door && GameManager.instance.currentItems < 3) return;
-
-           
-    //        if (Input.GetKey(KeyCode.E))
-    //        {
-    //            currentHoldTime += Time.deltaTime;
-
-                
-    //            GameManager.instance.UpdateLoading(currentHoldTime, holdTime);
-               
-
-    //            if (currentHoldTime >= holdTime)
-    //            {
-    //                PerformAction();
-    //            }
-    //        }
-    //        else
-    //        {
-                
-    //            if (currentHoldTime > 0)
-    //            {
-    //                currentHoldTime = 0f;
-    //                GameManager.instance.StopLoading(); 
-    //            }
-    //        }
-    //    }
-    //}
 
     public void PerformAction()
     {
-        //isCollected = true;
-        //isHintShowing = false;
+      
         GameManager.instance.StopLoading();
         GameManager.instance.HideHint();
 
@@ -112,46 +26,7 @@ public class InteractableObject : MonoBehaviour
         }
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        isPlayerNearby = true;
-    //        currentHoldTime = 0f;
-
-
-    //        if (type == ObjectType.Item) GameManager.instance.ShowHint("Giữ E để nhặt");
-    //        else if (type == ObjectType.Door)
-    //        {
-    //            if (GameManager.instance.currentItems >= 3) GameManager.instance.ShowHint("Vật phẩm: 3/3\nGiữ E để thoát");
-    //            else GameManager.instance.ShowHint($"Chưa đủ đồ ({GameManager.instance.currentItems}/3)");
-    //        }
-    //    }
-    //}
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        isPlayerNearby = false;
-    //        currentHoldTime = 0f;
-    //        GameManager.instance.HideHint();
-    //        GameManager.instance.StopLoading();
-    //        isHintShowing = false;
-    //    }
-    //}
-    //void ShowInteractionHint()
-    //{
-    //    if (type == ObjectType.Item)
-    //        GameManager.instance.ShowHint("Giữ E để nhặt");
-    //    else if (type == ObjectType.Door)
-    //    {
-    //        if (GameManager.instance.currentItems >= 3)
-    //            GameManager.instance.ShowHint("Vật phẩm: 3/3\nGiữ E để thoát");
-    //        else
-    //            GameManager.instance.ShowHint($"Chưa đủ đồ ({GameManager.instance.currentItems}/3)");
-    //    }
-    //}
+    
     public string GetHintText()
     {
         if (type == ObjectType.Item) return "Giữ E để nhặt";
