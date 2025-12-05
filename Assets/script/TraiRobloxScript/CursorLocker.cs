3107559290d3d597b6795a87be202ebc81ce6c9a
Hieu1904c <nguyeenshieeus@gmail.com>
2025-12-06 02:17:22 +0700

    hd
    rye

---
+++ Assets/script/TraiRobloxScript/CursorLocker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CursorLocker : MonoBehaviour
+{
+    void Start()
+    {
+        // Dòng này để khóa con chuột vào giữa màn hình
+        Cursor.lockState = CursorLockMode.Locked;
+        // Dòng này để ẩn con chuột đi
+        Cursor.visible = false;
+    }
+}
