f4fa8e62ed93ce45ee2f75cfbb2e6ec16943ff86
HaFo64 <trihao331166@gmail.com>
2025-12-06 00:38:36 +0700

    menu game
    menu

---
+++ Assets/script/TraiRobloxScript/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Bắt buộc phải có dòng này để chuyển cảnh
+
+public class MainMenuController : MonoBehaviour
+{
+    // Hàm này sẽ chạy khi bấm nút Play
+    public void PlayGame()
+    {
+        // Load scene có tên là "MainMap" như bạn yêu cầu
+        SceneManager.LoadScene("MainMap");
+    }
+
+    // Hàm này sẽ chạy khi bấm nút Exit
+    public void QuitGame()
+    {
+        Debug.Log("Đã thoát game!"); // Dòng này để test trong Editor cho biết là code đã chạy
+        Application.Quit();
+    }
+}
