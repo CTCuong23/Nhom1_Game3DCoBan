f36a136d262f44108588a2ed033dd56864f2265f
Hoangthong <thongktpop112@gmail.com>
2025-12-05 23:04:28 +0700

    XongMainMap

---
+++ Assets/script/AudioScript/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SoundThunder : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float delay = 5f; 
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        InvokeRepeating("PlaySound", 0f, delay);
+    }
+
+    void PlaySound()
+    {
+        audioSource.Play();
+    }
+}
