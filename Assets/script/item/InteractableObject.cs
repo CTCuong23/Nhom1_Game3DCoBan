cf0beaff4b8b800344b9033f7b2c40419026ddb6
Hieu1904c <nguyeenshieeus@gmail.com>
2025-12-07 04:54:27 +0700

    yrr
    rur

--- Assets/script/item/InteractableObject.cs
+++ Assets/script/item/InteractableObject.cs
@@ -6,16 +6,19 @@ public class InteractableObject : MonoBehaviour
     public enum ObjectType { Item, Door }
     public ObjectType type;
 
+    // --- MỚI: THÊM LOẠI VẬT PHẨM ---
+    public enum ItemType { None, Battery, KeyCard, Chip }
+    [Header("Loại vật phẩm (Chỉ chỉnh nếu Type là Item)")]
+    public ItemType specificItemType;
+
     [Header("Cài đặt")]
     public float holdTime = 2f;
 
-    // --- KHÔI PHỤC LẠI PHẦN CỬA CINEMATIC ---
-    [Header("Cài đặt cho Cửa (Chỉ dùng khi type là Door)")]
-    public Animator doorAnimator;      // Kéo Animator cửa vào đây
-    public Collider doorBlockCollider; // Kéo Collider chặn cửa vào đây
+    [Header("Cài đặt cho Cửa")]
+    public Animator doorAnimator;
+    public Collider doorBlockCollider;
     public PlayableDirector timelineDirector;
 
-    // Hàm này được script PlayerInteraction gọi để lấy nội dung hiển thị
     public string GetHintText()
     {
         if (type == ObjectType.Item)
@@ -24,14 +27,13 @@ public class InteractableObject : MonoBehaviour
         if (type == ObjectType.Door)
         {
             if (GameManager.instance.currentItems >= 3)
-                return "Vật phẩm: 3/3\nGiữ E để mở";
+                return "Đủ vật phẩm\nGiữ E để mở";
             else
                 return $"Chưa đủ đồ ({GameManager.instance.currentItems}/3)";
         }
         return "";
     }
 
-    // Hàm này được script PlayerInteraction gọi khi đã giữ đủ thời gian
     public void PerformAction()
     {
         GameManager.instance.StopLoading();
@@ -39,16 +41,15 @@ public class InteractableObject : MonoBehaviour
 
         if (type == ObjectType.Item)
         {
-            GameManager.instance.CollectItem();
+            // --- THAY ĐỔI: GỌI HÀM ANIMATION MỚI ---
+            GameManager.instance.CollectItemAnimated(specificItemType);
             Destroy(gameObject);
         }
         else if (type == ObjectType.Door)
         {
-            // 1. Mở cửa (Animation Cửa)
             if (doorAnimator != null) doorAnimator.SetTrigger("Open");
             if (doorBlockCollider != null) doorBlockCollider.enabled = false;
 
-            // 2. BẮT ĐẦU CUTSCENE (Giao lại cho GameManager lo)
             if (timelineDirector != null)
             {
                 GameManager.instance.StartEndingSequence(timelineDirector);
