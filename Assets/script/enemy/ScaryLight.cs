a0062d948330fc3e7b3f60f0286df12067b533ce
CTCuong23 <phucuong10082003@gmail.com>
2025-12-10 01:27:19 +0700

    Add Aura

---
+++ Assets/script/enemy/ScaryLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ScaryLight : MonoBehaviour
+{
+    Light myLight;
+    [SerializeField] float minIntensity = 2f; // Độ sáng thấp nhất
+    [SerializeField] float maxIntensity = 5f; // Độ sáng cao nhất
+    [SerializeField] float speed = 2f;        // Tốc độ nhấp nháy
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // Hàm PingPong giúp giá trị chạy qua lại giữa 0 và 1 liên tục
+        myLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, Mathf.PingPong(Time.time * speed, 1));
+    }
+}
