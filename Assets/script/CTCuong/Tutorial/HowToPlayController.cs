a17fd3e3b7cd3f121ed549b11fed36192fba021f
CTCuong23 <phucuong10082003@gmail.com>
2025-12-13 02:52:48 +0700

    Add tutorial panel

---
+++ Assets/script/CTCuong/Tutorial/HowToPlayController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class HowToPlayController : MonoBehaviour
+{
+    [SerializeField] private GameObject howToPanel;
+    [SerializeField] private GameObject hintText;
+    [SerializeField] private float hintDuration = 5f;
+
+    private bool firstCloseDone = false;
+
+    void Start()
+    {
+        // Khi vào game, panel mở sẵn
+        howToPanel.SetActive(true);
+
+        // Hint sẽ tự ẩn sau 5s
+        if (hintText != null)
+            Invoke(nameof(HideHint), hintDuration);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            bool isOpen = howToPanel.activeSelf;
+            howToPanel.SetActive(!isOpen);
+
+            // Khi người chơi bấm H lần đầu → hint chưa bị ẩn thì ẩn luôn cho gọn
+            if (!firstCloseDone)
+            {
+                firstCloseDone = true;
+                if (hintText != null) hintText.SetActive(false);
+            }
+        }
+    }
+
+    void HideHint()
+    {
+        if (hintText != null)
+            hintText.SetActive(false);
+    }
+}
